3b25c787f06c61da70c5e19f6cfd56243f27a91d
Florian CUNY <poslovitch@bentobox.world>
2019-10-06 11:18:52 +0200

    Initial setup

---
+++ src/main/java/world/bentobox/bank/Bank.java
@@ -0,0 +1,20 @@
+package world.bentobox.bank;
+
+import world.bentobox.bentobox.api.addons.Addon;
+
+/**
+ * Main class of the Bank addon.
+ * @author Poslovitch
+ */
+public class Bank extends Addon {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
