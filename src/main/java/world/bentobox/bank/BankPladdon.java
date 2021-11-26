24cf700f87571138c61027cbfbbfed1fa352bb7b
tastybento <tastybento@wasteofplastic.com>
2021-11-26 12:07:35 -0800

    Java 16 and enable Pladdon.

---
+++ src/main/java/world/bentobox/bank/BankPladdon.java
@@ -0,0 +1,16 @@
+package world.bentobox.bank;
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.Pladdon;
+
+/**
+ * @author tastybento
+ *
+ */
+public class BankPladdon extends Pladdon {
+
+    @Override
+    public Addon getAddon() {
+        return new Bank();
+    }
+
+}
